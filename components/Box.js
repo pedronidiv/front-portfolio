6e112eda2ab523985ff1fe133830ab4568e399e2
pedronidev <lucaspedronid@gmail.com>
2020-07-29 13:31:01 -0300

    more styling options

--- components/Box.js
+++ components/Box.js
@@ -1,20 +1,50 @@
 import React from "react";
 import styled from "styled-components";
 
-const Box = ({ aside, children }) => {
+const Box = ({
+  aside,
+  children,
+  direction = "top left",
+  asideProps = {},
+  centerProps = {},
+  bottomProps = {},
+  bottom,
+}) => {
   return (
-    <StyledBoxWrapper>
+    <StyledBoxWrapper className={`${direction}`}>
       <StyledBox>
-        <StyledBoxAside>{aside}</StyledBoxAside>
-        <StyledBoxCenter>{children}</StyledBoxCenter>
+        <StyledBoxMain>
+          <StyledBoxAside {...asideProps}>{aside}</StyledBoxAside>
+          <StyledBoxCenter {...centerProps}>{children}</StyledBoxCenter>
+        </StyledBoxMain>
+        {bottom && <StyledBoxBottom {...bottomProps}>{bottom}</StyledBoxBottom>}
       </StyledBox>
     </StyledBoxWrapper>
   );
 };
 
+const StyledBoxBottom = styled.div`
+  flex: 0 0 100%;
+  max-width: 100%;
+  width: 100%;
+`;
+const StyledBoxMain = styled.div`
+  display: flex;
+`;
+const StyledBox = styled.section`
+  background-color: #191919;
+  color: rgba(255, 255, 255, 0.7);
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  padding: 64px;
+  box-shadow: 0 0 30px black;
+  flex-wrap: wrap;
+`;
+
 const StyledBoxCenter = styled.div`
   padding-left: 64px;
-`
+`;
 
 const StyledBoxWrapper = styled.section`
   position: relative;
@@ -22,16 +52,40 @@ const StyledBoxWrapper = styled.section`
   margin-right: auto;
   width: var(--container-width);
   max-width: 100%;
-`;
-
-const StyledBox = styled.section`
-  background-color: #191919;
-  color: rgba(255, 255, 255, 0.7);
   display: flex;
-  width: 80%;
-  border-top-left-radius: 24px;
-  padding: 64px;
-  box-shadow: 0 0 30px black;
+  &.right {
+    justify-content: flex-end;
+    ${StyledBox} {
+      ${StyledBoxMain} {
+        flex-direction: row-reverse;
+      }
+      ${StyledBoxCenter} {
+        padding-left: 0;
+        padding-right: 64px;
+        text-align: right;
+      }
+    }
+  }
+  &.top.left {
+    ${StyledBox} {
+      border-top-left-radius: 24px;
+    }
+  }
+  &.top.right {
+    ${StyledBox} {
+      border-top-right-radius: 24px;
+    }
+  }
+  &.bottom.left {
+    ${StyledBox} {
+      border-bottom-left-radius: 24px;
+    }
+  }
+  &.bottom.right {
+    ${StyledBox} {
+      border-bottom-right-radius: 24px;
+    }
+  }
 `;
 
 const StyledBoxAside = styled.aside`
@@ -39,6 +93,5 @@ const StyledBoxAside = styled.aside`
   flex: 0 0 200px;
   text-align: right;
 `;
-
 
 export default Box;
