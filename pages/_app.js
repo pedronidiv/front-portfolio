ad4639a7239b50b1ab8a471cf4300e00934697c6
pedronidev <lucaspedronid@gmail.com>
2020-07-30 11:23:45 -0300

    favicons

--- pages/_app.js
+++ pages/_app.js
@@ -13,8 +13,33 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Lucas Pedroni</title>
-
+        <title>Lucas Pedroni, desenvolvedor full stack</title>
+        <meta
+          name="description"
+          content="Dando vida a ideias. Desenvolvimento de sites, aplicativos e sistemas sob medida. Tecnologias utilizadas: PHP; Laravel; JavaScript; Angular; Ionic; React e muito mais."
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/img/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/img/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/img/favicon/favicon-16x16.png"
+        />
+        <link rel="mask-icon" href="/img/favicon/safari-pinned-tab.svg" color="#000000" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="msapplication-TileImage" content="/img/favicon/mstile-144x144.png" />
+        <meta name="theme-color" content="#000000" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;800&display=swap"
           rel="stylesheet"
@@ -23,6 +48,12 @@ export default function App({ Component, pageProps }) {
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/gh/konpa/devicon@master/devicon.min.css"
         />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-121297636-1"
+        ></script>
+        <script src="/js/google.analyics.js"></script>
       </Head>
       <Component {...pageProps} />
     </>
